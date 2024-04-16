1d74edfc7d1a93dc56b0a04a59110712d6bc29eb
Branimir Hrsto <branimir.hrsto@gmail.com>
2024-04-16 10:53:22 +0200

    Dodavanje Detail page

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
@@ -14,8 +16,14 @@ namespace RunGroupWebApp.Controllers
 
         public IActionResult Index()
         {
-            var clubs = _context.Clubs.ToList();
+            List<Club> clubs = _context.Clubs.ToList();
             return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Club club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
         }
     }
 }
---
+++ Interface/IRaceRepository.cs
@@ -0,0 +1,16 @@
+﻿using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Interface
+{
+    public interface IRaceRepository
+    {
+        Task<IEnumerable<Race>> GetAll();
+        Task<Race> GetByIdAsync(int id);
+        Task<IEnumerable<Race>> GetRaceByCity(string city);
+
+        bool Add(Race race);
+        bool Update(Race race);
+        bool Delete(Race race);
+        bool Save();
+    }
+}
